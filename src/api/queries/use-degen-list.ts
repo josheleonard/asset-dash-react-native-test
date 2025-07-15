2b2daa21daa822ed845492996544319eb2a6be7b
Josh E Leonard <30185185+josheleonard@users.noreply.github.com>
2025-07-14 20:04:42 -0600

    fix: randomize prices for random assets

--- src/api/queries/use-degen-list.ts
+++ src/api/queries/use-degen-list.ts
@@ -8,7 +8,33 @@ export const degenListQueryKey = ["degen_list"];
 
 export const degenListQueryFn = async (): Promise<DegenListData> => {
   const degenListResponse = await fetch(DEGEN_LIST_ENDPOINT);
-  return degenListResponse.json();
+  const degenList: DegenListData = await degenListResponse.json();
+  // Update prices for some tokens with random values
+  return degenList.map((item) => {
+    const shouldUpdate = Math.random() > 0.2;
+    if (!shouldUpdate) {
+      return item;
+    }
+
+    // set the price to a new random value and update the price change percentages
+    const oldPrice = item.price_usd;
+    const newPrice = Math.random() * 0.01;
+
+    const newPriceChangePercent = ((newPrice - oldPrice) / oldPrice) * 100;
+
+    return {
+      ...item,
+      price_usd: newPrice,
+      price_change_percent: {
+        h1: (item.price_change_percent?.h1 || 0) + newPriceChangePercent,
+        h24: (item.price_change_percent?.h24 || 0) + newPriceChangePercent,
+        h4: (item.price_change_percent?.h4 || 0) + newPriceChangePercent,
+        h8: (item.price_change_percent?.h8 || 0) + newPriceChangePercent,
+        m30: (item.price_change_percent?.m30 || 0) + newPriceChangePercent,
+        m5: (item.price_change_percent?.m5 || 0) + newPriceChangePercent,
+      },
+    };
+  });
 };
 
 export const useDegenList = () => {
